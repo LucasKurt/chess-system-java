c31e1e017953a65d80a44fa0dac07d10823fb3fa
Lucas Côrte <lucascorte@outlook.com>
2022-01-20 18:31:13 -0300

    Little improvement in board printing

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Colored shell (y/n)? ");
+		char shell = sc.next().charAt(0);
+
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		UI.printBoard(chessMatch.getPieces(), shell);
+
+		sc.close();
 	}
 
 }
