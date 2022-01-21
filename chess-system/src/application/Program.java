2bc1382358dcc2120a7e984fce88e956462bf072
Lucas Côrte <lucascorte@outlook.com>
2022-01-20 21:22:28 -0300

    Moving pieces

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Scanner;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
@@ -10,13 +12,25 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Colored shell (y/n)? ");
-		char shell = sc.next().charAt(0);
+//		System.out.print("Colored shell (y/n)? ");
+//		char shell = sc.next().charAt(0);
 
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces(), shell);
-
-		sc.close();
+		
+		
+		while (true) {
+//			UI.printBoard(chessMatch.getPieces(), shell);
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
